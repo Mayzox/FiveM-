ea39934c1115c64a38caad0663cf3d6c79e97810
Mayzox <125056047+Mayzox@users.noreply.github.com>
2024-08-24 22:52:41 +0200

    Add files via upload

---
+++ Loading_Mayzox/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'By Mizian'
+description 'Loading screen for DreamLand RP'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'index.html',
+    'style/main.css',
+    'script/main.js',
+    'audio/loadingmzn.mp3',
+    'audio/go.mp3',
+    'audio/salvatore.mp3',
+    'img/logod.png',
+    'img/logoN.png',
+    'img/member1.png',
+    'img/background.png',
+    'img/backgroundN.png',
+    'img/logo.png',
+    'img/logo1.png',
+    'img/member.png',
+    'img/member2.png',
+    'img/member3.png',
+    'img/background.png',
+    'img/arrow.svg',
+    'img/music.png',
+    'img/news.png',
+    'img/back.svg',
+    'img/pause.svg',
+    'img/play.svg',
+    'img/forward.svg'
+}
